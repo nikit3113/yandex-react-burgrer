7ebc5bb8c74871c0b876a26ccf8d237ae076feeb
Nikita Shukalin <shnd@rielta.com>
2022-04-25 19:36:22 +0300

    add order details and ingredient data components

---
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import styles from './ingredient-details.module.css'
+
+function IngredientDetails(props) {
+  return (
+    <div className={styles.root}>
+      <img src={props.ingredient.image_large} alt={props.ingredient.name}/>
+      <p className={'text_type_main-medium pt-4 pb-8'}>{props.ingredient.name}</p>
+      <div className={`${styles.info} mb-15`}>
+        <div className={styles.infoElem}>
+          <span className="text_type_main-default mb-2">Калории,ккал</span>
+          <span className="text_type_digits-default">{props.ingredient.calories}</span>
+        </div>
+        <div className={styles.infoElem}>
+          <span className="text_type_main-default mb-2">Белки, г</span>
+          <span className="text_type_digits-default">{props.ingredient.proteins}</span>
+        </div>
+        <div className={styles.infoElem}>
+          <span className="text_type_main-default mb-2">Жиры, г</span>
+          <span className="text_type_digits-default">{props.ingredient.fat}</span>
+        </div>
+        <div className={styles.infoElem}>
+          <p className="text_type_main-default mb-2">Углеводы, г</p>
+          <p className="text_type_digits-default">{props.ingredient.carbohydrates}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+IngredientDetails.propTypes = {
+  ingredient: PropTypes.shape({
+    _id: PropTypes.string,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    proteins: PropTypes.number,
+    fat: PropTypes.number,
+    carbohydrates: PropTypes.number,
+    calories: PropTypes.number,
+    price: PropTypes.number,
+    image: PropTypes.string,
+    image_mobile: PropTypes.string,
+    image_large: PropTypes.string,
+    __v: PropTypes.number
+  }).isRequired
+};
+
+export default IngredientDetails;
---
+++ src/components/order-details/order-details.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './order-details.module.css';
+import { CheckMarkIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import PropTypes from "prop-types";
+import IngredientDetails from "../ingredient-details/ingredient-details";
+
+function OrderDetails(props) {
+  return (
+    <div style={styles.root}>
+      <span className={'text_type_digits-large mb-8'}>{props.orderNum}</span>
+      <p className={'text_type_main-medium mb-15'}>идентификатор заказа</p>
+      <div className={styles.image}>
+        <CheckMarkIcon/>
+      </div>
+      <p className={'text_type_main-default mt-15'}>Ваш заказ начали готовить</p>
+      <p className={'text_type_main-default mb-30 mt-2'}>Дождитесь готовности на орбитальной станции</p>
+    </div>
+  )
+}
+IngredientDetails.propTypes = {
+  orderNum: PropTypes.string
+}
+
+export default OrderDetails;

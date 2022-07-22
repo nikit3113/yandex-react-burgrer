3bfd3d4f03fa8a60a594d013f5b92b61913a4c68
Nikita Shukalin <shnd@rielta.com>
2022-07-22 16:58:10 +0300

    Change app-header to functional component. Fix this component

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,48 +1,39 @@
-import React from "react";
+import React, {useCallback} from "react";
 import {Logo, BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import headerStyle from './app-header.module.css';
+import {useHistory} from 'react-router-dom';
 
 const BUTTON_MARGIN = 'p-5 mt-4 mb-4 mr-2';
 
-class MenuItem extends React.Component {
-  render() {
-    return (
-      <button className={[headerStyle.menuItem, BUTTON_MARGIN].join(' ')} type='button'>
-        {this.props.icon}
+function MenuItem({icon, text, ...other}) {
+  return (
+      <button className={[headerStyle.menuItem, BUTTON_MARGIN].join(' ')} type='button' {...other}>
+        {icon}
         <p className="text text_type_main-default ml-2">
-          {this.props.text}
+          {text}
         </p>
       </button>
-    );
-  }
+  );
 }
 
-class Button extends React.Component {
-  render() {
-    return (
-      <button className={[headerStyle.menuItem, BUTTON_MARGIN].join(' ')} type='button'>
-        {this.props.icon}
-        <p className="text text_type_main-default ml-2">
-          {this.props.text}
-        </p>
-      </button>
-    );
-  }
-}
-
-class AppHeader extends React.Component {
-  render() {
-    return (
-      <header className={headerStyle.header}>
-        <nav className={headerStyle.menu}>
-          <MenuItem text={'Конструктор'} icon={<BurgerIcon type={"primary"}/>}/>
-          <MenuItem text={'Лента заказов'} icon={<ListIcon type={"primary"}/>}/>
-        </nav>
-        <Logo></Logo>
-        <Button text={'Личный кабинет'} icon={<ProfileIcon type={"primary"}/>}/>
-      </header>
-    );
-  }
+function AppHeader() {
+  const history = useHistory();
+  const login = useCallback(
+    () => {
+      history.replace({pathname: '/login'});
+    },
+    [history]
+  );
+  return (
+    <header className={headerStyle.header}>
+      <nav className={headerStyle.menu}>
+        <MenuItem text={'Конструктор'} icon={<BurgerIcon type={"primary"}/>}/>
+        <MenuItem text={'Лента заказов'} icon={<ListIcon type={"primary"}/>}/>
+      </nav>
+      <Logo/>
+      <MenuItem text={'Личный кабинет'} icon={<ProfileIcon type={"primary"}/>} onClick={login}/>
+    </header>
+  );
 }
 
 export default AppHeader;

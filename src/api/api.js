c7e32f4c340a6ca088349b88f1e184c1bc4bf14d
Nikita Shukalin <shnd@rielta.com>
2022-07-26 13:37:14 +0300

    Add new api for reset password and registration

--- src/api/api.js
+++ src/api/api.js
@@ -21,3 +21,39 @@ export async function postOrder(data = {}) {
   });
   return checkResponse(response);
 }
+
+// Сброс пароля:
+export async function passwordForgot(email) {
+  const response = await fetch(BASE_URL + '/password-reset', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({email}),
+  });
+  return checkResponse(response);
+}
+
+// Сброс пароля:
+export async function passwordReset(password, token) {
+  const response = await fetch(BASE_URL + '/password-reset/reset', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({password, token}),
+  });
+  return checkResponse(response);
+}
+
+// Регистрация:
+export async function register(email, password, name) {
+  const response = await fetch(BASE_URL + '/auth/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({email, password, name}),
+  });
+  return checkResponse(response);
+}

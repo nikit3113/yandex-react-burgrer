d22c1c04c42e10f828222004c08d0cd5c9168339
Nikita Shukalin <shnd@rielta.com>
2022-08-03 11:17:00 +0300

    Add loader to await order

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import styles from './order-details.module.css';
 import {useSelector} from "react-redux";
+import Loader from "../loader/loader";
 
 function OrderDetails() {
   const {orderNumber, orderNumberFailed, orderNumberRequest} = useSelector(store => store.common);
   return (
     <div className={styles.root}>
-      {orderNumberRequest ?
-        <p className={'text_type_main-medium mb-15'}>Отправляем ваш заказ на кухню...</p> :
-        orderNumberFailed ?
-          <p className={'text_type_main-medium mb-15'}>К сожалению произошла какая то ошибка...</p> :
+      {orderNumberRequest ? (
+        <div className={styles.loader + ' mb-15'}>
+          <p className={'text_type_main-medium'}>Отправляем ваш заказ на кухню...</p>
+          <Loader/>
+        </div>
+      ) : orderNumberFailed ?
+        (<p className={'text_type_main-medium mb-15'}>К сожалению произошла какая то ошибка...</p>
+        ) : (
           <>
             <span className={`${styles.title} text_type_digits-large mb-8`}>{orderNumber}</span>
             <p className={'text_type_main-medium mb-15'}>идентификатор заказа</p>
@@ -17,7 +22,7 @@ function OrderDetails() {
             <p className={'text_type_main-default mt-15'}>Ваш заказ начали готовить</p>
             <p className={`${styles.waitingText} text_type_main-default mb-30 mt-2`}>Дождитесь готовности на орбитальной
               станции</p>
-          </>
+          </>)
       }
     </div>
   )

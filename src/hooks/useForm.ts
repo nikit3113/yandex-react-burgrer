60171e1cb8c379eb7272cc3f8dbf71b31f563893
Nikita <nikit3113@gmail.com>
2022-09-29 22:55:57 +0300

    Fix useForm event type

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,9 +1,9 @@
-import {useState} from "react";
+import React, {useState} from "react";
 
 export function useForm(inputValues: any) {
   const [values, setValues] = useState(inputValues);
 
-  const handleChange = (event: any) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const {value, name} = event.target;
     setValues({...values, [name]: value});
   };

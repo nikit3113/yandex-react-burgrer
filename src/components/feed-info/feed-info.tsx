a0de6f1ac277143b51baf044799a584e3492a70e
Nikita Shukalin <shukalin.nikita@luckymonkeys.games>
2022-10-26 15:00:48 +0300

    Fix Layout

--- src/components/feed-info/feed-info.tsx
+++ src/components/feed-info/feed-info.tsx
@@ -9,22 +9,48 @@ type TFeedInfoProps = {
 }
 
 const FeedInfo: FC<TFeedInfoProps> = ({total, totalToday, readyOrders, inWorkOrders}) => {
+  const chunkSize = 10;
+  let chunkReadyArray = [];
+  let chunkInWorkArray = [];
+  if (readyOrders){
+    let j=0;
+    for (let i = 0; i < readyOrders.length; i += chunkSize) {
+      chunkReadyArray[j] = readyOrders.slice(i, i + chunkSize);
+      j++;
+    }
+  }
+  if (inWorkOrders){
+    let j=0;
+    for (let i = 0; i < inWorkOrders.length; i += chunkSize) {
+      chunkInWorkArray[j] = inWorkOrders.slice(i, i + chunkSize);
+      j++;
+    }
+  }
+
   return (
     <div className={feedStyles.main}>
       <div className={feedStyles.top_block}>
         <div className={feedStyles.container}>
           <span className="text_type_main-medium">Готовы:</span>
-          <div className={feedStyles.scrollView}>
-            {readyOrders?.map((number) => (
-              <p className={"text_type_digits-default text_color_success"} key={number}>{number}</p>
-            ))}
+          <div className={feedStyles.chunk}>
+          {chunkReadyArray?.map((chunk)=>(
+            <div className={feedStyles.block}>
+              {chunk?.map((number) => (
+                <p className={"text_type_digits-default text_color_success"} key={number}>{number}</p>
+                ))}
+            </div>
+          ))}
           </div>
         </div>
         <div className={feedStyles.container}>
           <span className="text_type_main-medium">В работе:</span>
-          <div  className={feedStyles.scrollView}>
-            {inWorkOrders?.map((number) => (
-              <p className={"text_type_digits-default"} key={number}>{number}</p>
+          <div className={feedStyles.chunk}>
+            {chunkInWorkArray?.map((chunk)=>(
+              <div className={feedStyles.block}>
+                {chunk?.map((number) => (
+                  <p className={"text_type_digits-default text_color_success"} key={number}>{number}</p>
+                ))}
+              </div>
             ))}
           </div>
         </div>

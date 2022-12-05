d571a207379f83bde83d5907c01feea5f3d3890c
Nikita Shukalin <shukalin.nikita@luckymonkeys.games>
2022-12-05 22:39:23 +0300

    Review add more jest tests fore reducers

--- src/services/reducers/user.test.js
+++ src/services/reducers/user.test.js
@@ -1,7 +1,19 @@
 import {
   LOGIN_USER_REQUEST,
   LOGIN_USER_SUCCESS,
-  LOGIN_USER_FAILED, GET_USER_REQUEST, GET_USER_SUCCESS, GET_USER_FAILED,
+  LOGIN_USER_FAILED,
+  GET_USER_REQUEST,
+  GET_USER_SUCCESS,
+  GET_USER_FAILED,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAILED,
+  UPDATE_USER_SUCCESS,
+  LOGOUT,
+  AUTH_CHECKOUT_SUCCESS,
+  AUTH_CHECKOUT_IS_END,
+  UPDATE_USER_REQUEST,
+  UPDATE_USER_FAILED
 } from '../constants/user';
 import {userReducer} from './user';
 
@@ -37,10 +49,10 @@ describe('Проверка редьюсера userReducer', () => {
   it('Проверка начального состояния', () => {
     expect(userReducer(undefined, {})).toEqual(initialState);
   })
-  it('Проверка редьюсера. IS_REQUESTING', () => {
+  it('Проверка редьюсера. LOGIN_USER_REQUEST', () => {
     expect(userReducer(initialState, {type: LOGIN_USER_REQUEST})).toEqual({...initialState, loginUserRequest: true});
   })
-  it('Проверка редьюсера. IS_SUCCESSFUL', () => {
+  it('Проверка редьюсера. LOGIN_USER_SUCCESS', () => {
     expect(userReducer(initialState, {type: LOGIN_USER_SUCCESS, user: testUser})).toEqual({
         ...initialState,
         user: testUser,
@@ -49,11 +61,34 @@ describe('Проверка редьюсера userReducer', () => {
       }
     )
   })
-  it('Проверка редьюсера. IS_FAILED', () => {
+  it('Проверка редьюсера. LOGIN_USER_FAILED', () => {
     expect(userReducer(initialState, {type: LOGIN_USER_FAILED, error: 'error'})).toEqual({
       ...initialState,
       loginUserRequest: false,
       loginUserError: 'error',
+    })
+  })
+  it('Проверка редьюсера. REGISTER_USER_REQUEST', () => {
+    expect(userReducer(initialState, {type: REGISTER_USER_REQUEST})).toEqual({
+      ...initialState,
+      registerUserRequest: true,
+      registerUserError: '',
+    });
+  })
+  it('Проверка редьюсера. REGISTER_USER_SUCCESS', () => {
+    expect(userReducer(initialState, {type: REGISTER_USER_SUCCESS, user: testUser})).toEqual({
+        ...initialState,
+        user: testUser,
+        registerUserRequest: false,
+        registerUserError: '',
+      }
+    )
+  })
+  it('Проверка редьюсера. REGISTER_USER_FAILED', () => {
+    expect(userReducer(initialState, {type: REGISTER_USER_FAILED, error: 'error'})).toEqual({
+      ...initialState,
+      registerUserRequest: false,
+      registerUserError: 'error',
     })
   })
   it('Проверка редьюсера. GET_USER_REQUEST', () => {
@@ -78,6 +113,47 @@ describe('Проверка редьюсера userReducer', () => {
       getUserRequest: false,
       getUserError: 'error',
       user: null,
+    })
+  })
+  it('Проверка редьюсера. UPDATE_USER_REQUEST', () => {
+    expect(userReducer(initialState, {type: UPDATE_USER_REQUEST})).toEqual({
+      ...initialState,
+      updateUserRequest: true,
+      updateUserError: '',
+    });
+  })
+  it('Проверка редьюсера. UPDATE_USER_SUCCESS', () => {
+    expect(userReducer(initialState, {type: UPDATE_USER_SUCCESS, user: testUser})).toEqual({
+        ...initialState,
+        user: testUser,
+        updateUserRequest: false,
+        updateUserError: '',
+      }
+    )
+  })
+  it('Проверка редьюсера. UPDATE_USER_FAILED', () => {
+    expect(userReducer(initialState, {type: UPDATE_USER_FAILED, error: 'error'})).toEqual({
+      ...initialState,
+      updateUserRequest: false,
+      updateUserError: 'error',
+    })
+  })
+  it('Проверка редьюсера. LOGOUT', () => {
+    expect(userReducer(initialState, {type: LOGOUT})).toEqual({
+      ...initialState,
+      user: null,
+    })
+  })
+  it('Проверка редьюсера. AUTH_CHECKOUT_SUCCESS', () => {
+    expect(userReducer(initialState, {type: AUTH_CHECKOUT_SUCCESS})).toEqual({
+      ...initialState,
+      authIsChecked: true,
+    })
+  })
+  it('Проверка редьюсера. AUTH_CHECKOUT_IS_END', () => {
+    expect(userReducer(initialState, {type: AUTH_CHECKOUT_IS_END})).toEqual({
+      ...initialState,
+      authIsChecked: false,
     })
   })
 });
